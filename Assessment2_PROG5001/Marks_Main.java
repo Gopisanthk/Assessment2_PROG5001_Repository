211623c2240d0844404840dc7d82b60db32696d5
Gopisanthk <162999414+Gopisanthk@users.noreply.github.com>
2024-08-02 22:39:29 +1000

    Main class creation

---
+++ Assessment2_PROG5001/Marks_Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Marks_Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Marks_Main
+{
+    public static void main(String[] args) 
+    {
+        
+        
+    }
+}
